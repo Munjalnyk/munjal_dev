3c31d3095957fe03def1e92f36f4487351c8258b
yorocoboy1 <82338067+yorocoboy1@users.noreply.github.com>
2023-02-19 01:14:34 +0530

    rfgtrfg

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,7 +186,7 @@ function buttonMoveLeft(){
 function buttonMoveRight(){
 
     const button = document.getElementById('submit-btn');
-    button.style.transform = 'translateX(130%)';
+    button.style.transform = 'translateX(120%)';
 
 };
 
