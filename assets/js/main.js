75f23a69893479838383e6679d4fd8a86b79b591
yorocoboy1 <82338067+yorocoboy1@users.noreply.github.com>
2023-02-19 00:24:22 +0530

    wsxaq

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,22 +132,7 @@ var swiperTestimonial = new Swiper(".testimonial__container", {
         },
     },
 });
-const button = document.getElementById('move-btn');
-document.addEventListener('mousemove', function(event) {
-  const x = event.clientX;
-  const y = event.clientY;
-  const buttonRect = button.getBoundingClientRect();
-  const buttonX = buttonRect.left + (buttonRect.width / 2);
-  const buttonY = buttonRect.top + (buttonRect.height / 2);
-  const deltaX = x - buttonX;
-  const deltaY = y - buttonY;
-  const distance = Math.sqrt((deltaX * deltaX) + (deltaY * deltaY));
-  const maxDistance = 200;
-  const moveAmount = (distance / maxDistance) * 10;
-  const moveX = (deltaX / distance) * moveAmount;
-  const moveY = (deltaY / distance) * moveAmount;
-  button.style.transform = `translate(${moveX}px, ${moveY}px)`;
-});
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
 
