fa21255821910e1f34b40c639c23bae29a56b291
yorocoboy1 <82338067+yorocoboy1@users.noreply.github.com>
2023-02-19 00:55:10 +0530

    vfcrdxs

--- assets/js/main.js
+++ assets/js/main.js
@@ -132,29 +132,70 @@ var swiperTestimonial = new Swiper(".testimonial__container", {
         },
     },
 });
-$(document).ready(function() {
-    // When the submit button is clicked
-    $('#submit-button').click(function(e) {
-      // Check if any required fields are empty
-      var emptyFields = $('input[required], textarea[required]').filter(function() {
-        return $(this).val() === '';
-      });
-      
-      if (emptyFields.length) {
-        // Prevent the form from submitting
-        e.preventDefault();
-        
-        // Add a class to the submit button to move it sideways
-        $('#submit-button').addClass('button--invalid');
-        
-        // Remove the class when the user focuses on a required field
-        $('input[required], textarea[required]').on('focus', function() {
-          $('#submit-button').removeClass('button--invalid');
-        });
-      }
-    });
-  });
-  
+var a = 0;
+
+function mouseOver(){
+    
+    const name = document.forms['suForm']['name'].value;
+    const email = document.forms['suForm']['email'].value;
+    const pass = document.forms['suForm']['pass'].value;
+    const tick = document.querySelector('#check');
+
+    const emailCheck = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+
+
+    if((name == "" || !email.match(emailCheck) || pass == "" == false) && a==0){
+    buttonMoveLeft();
+    a = 1;
+    return false;
+    } 
+
+    if((name == "" || !email.match(emailCheck) || pass == "" == false) && a==1){
+    buttonMoveRight();
+    a = 2;
+    return false;
+    } 
+
+    if((name == "" || !email.match(emailCheck) || pass == "" == false) && a==2){
+    buttonMoveLeft();
+    a = 1;
+    return false;
+    } 
+
+    else{
+
+        // document.getElementById('submit-btn').click();  
+        document.getElementById('submit-btn').style.cursor = 'pointer';
+        return false;
+    };
+
+};
+
+
+
+
+function buttonMoveLeft(){
+
+    const button = document.getElementById('submit-btn');
+    button.style.transform = 'translateX(-160%)';
+
+};
+
+
+function buttonMoveRight(){
+
+    const button = document.getElementById('submit-btn');
+    button.style.transform = 'translateX(0%)';
+
+};
+
+
+function resetBtn(){
+    const button = document.getElementById('submit-btn');
+    button.style.transform = 'translateX(0%)';
+};
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll("section[id]");
 
