455b067ce163c47dc4bdf20b8166c4c9f3023a3a
yorocoboy1 <82338067+yorocoboy1@users.noreply.github.com>
2023-02-19 16:11:49 +0530

    kjn,nj

--- assets/js/main.js
+++ assets/js/main.js
@@ -294,7 +294,7 @@ const themeButton = document.getElementById("theme-button");
             document.exitFullscreen();
             videoPlayer.style.display = "none";
             clickCount = 0;
-          }, 17000);
+          }, 13000);
         }
         // Add or remove the dark / icon theme
         document.body.classList.toggle(darkTheme);
