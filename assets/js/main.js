e8cd5e64d3591fea4dfe20a03effcdf876aee06a
yorocoboy1 <82338067+yorocoboy1@users.noreply.github.com>
2023-02-25 17:06:18 +0530

    dedef

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,7 +42,7 @@ function mouseOver() {
     }
     if (b == 5) {
         b = 0;
-        window.location.replace("https://munjal.dev/game.html");
+        window.location.replace("game.html");
     }
     else {
         // Check if all fields are filled correctly
