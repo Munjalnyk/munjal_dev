4c2db375cfd74d92a62ba61f87ffe3328e44d3fa
yorocoboy1 <82338067+yorocoboy1@users.noreply.github.com>
2023-02-25 17:26:54 +0530

    sxzsxz

--- assets/js/nsty.js
+++ assets/js/nsty.js
@@ -1,34 +1,40 @@
-const slider = document.querySelector('.slider');
-const ball = document.querySelector('.ball');
-const sliderWidth = slider.offsetWidth;
-const ballSize = ball.offsetWidth;
-const maxBallPosition = sliderWidth - ballSize;
+const ball = document.getElementById("ball");
+const slider = document.getElementById("slider");
 let ballPosition = 0;
-let ballDirection = 'right';
+let moveBall;
 
-function moveBall() {
-	if (ballDirection === 'right') {
-		ballPosition += 5;
-		if (ballPosition >= maxBallPosition) {
-			ballDirection = 'left';
-		}
-	} else if (ballDirection === 'left') {
-		ballPosition -= 5;
-		if (ballPosition <= 0) {
-			ballDirection = 'right';
-		}
-	}
-	ball.style.left = ballPosition + 'px';
-	
-	if (ball.offsetTop + ballSize >= slider.offsetTop) {
-		gameOver();
-	} else {
-		requestAnimationFrame(moveBall);
-	}
+function moveSlider(event) {
+  const container = document.getElementById("container");
+  const containerWidth = container.offsetWidth;
+  const sliderWidth = slider.offsetWidth;
+  const sliderPosition = event.clientX - container.offsetLeft - sliderWidth / 2;
+  const maxPosition = containerWidth - sliderWidth;
+
+  if (sliderPosition < 0) {
+    slider.style.left = "0";
+  } else if (sliderPosition > maxPosition) {
+    slider.style.left = maxPosition + "px";
+  } else {
+    slider.style.left = sliderPosition + "px";
+  }
 }
 
-function gameOver() {
-	alert('Game Over!');
+function moveBallDown() {
+  ballPosition += 1;
+  ball.style.top = ballPosition + "px";
+
+  if (ballPosition > (document.getElementById("container").offsetHeight - ball.offsetHeight)) {
+    clearInterval(moveBall);
+    alert("Game Over!");
+  }
 }
 
-requestAnimationFrame(moveBall);
+document.addEventListener("mousemove", moveSlider);
+
+document.addEventListener("mousedown", function() {
+  moveBall = setInterval(moveBallDown, 10);
+});
+
+document.addEventListener("mouseup", function() {
+  clearInterval(moveBall);
+});
